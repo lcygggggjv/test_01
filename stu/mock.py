b7c196332ad5ed050b4850b47e8d121adb82b632
lcygggggjv <1743998121@qq.com>
2023-06-18 17:25:44 +0800

    222;

--- stu/mock.py
+++ stu/mock.py
@@ -2,11 +2,12 @@
 
 import string
 import random
-
+from faker import Faker
 
 class Mock:
 
-    def mock_data(self, strs="cr"):
+    @staticmethod
+    def mock_data(strs="cr"):
 
         all_str = string.digits + string.ascii_letters
 
@@ -14,11 +15,25 @@ class Mock:
 
         return strs + ran_str
 
+    @staticmethod
+    def faker_name():
+
+        fk = Faker(locale=['zh_CN'])
+
+        sd = fk.name()
+        return sd
+
+    @staticmethod
+    def faker_str():
+
+        fs = Faker()
+        return fs.pystr(min_chars=2, max_chars=5)
+
 
 if __name__ == '__main__':
 
     cs = Mock()
 
-    SS = cs.mock_data()
+    SS = cs.faker_str()
 
     print(SS)
