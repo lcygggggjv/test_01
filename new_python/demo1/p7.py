15de2ec680496ace69ac924d7355b8598af9886d
lcygggggjv <1743998121@qq.com>
2023-07-22 23:28:25 +0800

    555522---5

--- new_python/demo1/p7.py
+++ new_python/demo1/p7.py
@@ -1,16 +1,56 @@
 import json
+from pymysql import *
+import pymysql
 
-list2 = ['{"k": 2}', {"e": 2}, '{"w": "k"}', ('y', 2), ('v', 5)]
+# list2 = ['{"k": 2}', {"e": 2}, '{"w": "k"}', ('y', 2), ('v', 5)]
+#
+# for s in list2:
+#
+#     if isinstance(s, str):
+#         w = json.loads(s)
+#     elif isinstance(s, tuple):
+#
+#         w = dict([s])
+#
+#     else:
+#         w = s
+#
+#     print(type(w), w)
 
-for s in list2:
 
-    if isinstance(s, str):
-        w = json.loads(s)
-    elif isinstance(s, tuple):
+def connect():
 
-        w = dict([s])
+    db = pymysql.connect(host='127.0.0.1', user='root', password='123456789', database='lcy1', charset='utf8')
 
-    else:
-        w = s
+    return db
 
-    print(type(w), w)
+
+db = connect()
+
+
+def create_database(db):
+
+    cursor = db.cursor()
+
+    sql = '''CREATE TABLE persons
+        (id INT PRIMARY KEY     NOT NULL,
+        name        TEXT        NOT NULL,
+        age          INT        NOT NULL,
+        address       CHAR(200),
+        salary        REAL);
+        '''
+
+    try:
+        cursor.execute(sql)
+
+        db.commit()
+
+        return True
+    except:
+
+        db.rollback()
+
+    return False
+
+
+create_database(db)
